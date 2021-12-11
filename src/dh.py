4933e8ae55663c9465244f77409f8422bd99b18a
dchampion <divaconhamdip@gmail.com>
2021-12-11 14:21:37 -0700

    Fix bug in generate_p, and add doc.

--- src/dh.py
+++ src/dh.py
@@ -9,116 +9,121 @@ min_p_bit_len = 2048
 secure_random = secrets.SystemRandom()
 
 def main():
-    start = time.perf_counter_ns()
+    print("Running tests...")
     p, q, g = generate_parameters(2048)
-    end = time.perf_counter_ns()
-    print((end - start) / 1000000)
-
-    start = time.perf_counter_ns()
     validate_parameters(p, q, g)
-    end = time.perf_counter_ns()
-    print((end - start) / 1000000)
+    print("validate_parameters() passed")
     
-    start = time.perf_counter_ns()
-    k_priv_1, k_pub_1 = gen_keypair(g, q, p)
-    end = time.perf_counter_ns()
-    print((end - start) / 1000000)
-
-    start = time.perf_counter_ns()
+    k_priv_1, k_pub_1 = generate_keypair(g, q, p)
     validate_pub_key(k_pub_1, q, p)
-    end = time.perf_counter_ns()
-    print((end - start) / 1000000)
+    print("validate_pub_key() 1 passed")
 
-    start = time.perf_counter_ns()
-    k_priv_2, k_pub_2 = gen_keypair(g, q, p)
-    end = time.perf_counter_ns()
-    print((end - start) / 1000000)
-
-    start = time.perf_counter_ns()
+    k_priv_2, k_pub_2 = generate_keypair(g, q, p)
     validate_pub_key(k_pub_2, q, p)
-    end = time.perf_counter_ns()
-    print((end - start) / 1000000)
+    print("validate_pub_key() 2 passed")
 
-    start = time.perf_counter_ns()
     k_secret1 = primes.fast_mod_exp(k_pub_2, k_priv_1, p)
-    end = time.perf_counter_ns()
-    print((end - start) / 1000000)
-
-    start = time.perf_counter_ns()
     k_secret2 = primes.fast_mod_exp(k_pub_1, k_priv_2, p)
-    end = time.perf_counter_ns()
-    print((end - start) / 1000000)
 
-    assert k_secret1 == k_secret2 # Keys should be hashed (see 14.6)
+    # Keys should be hashed (see 14.6)
+    assert k_secret1 == k_secret2, "Secrets don't match"
+    print("Secrets match; all tests passed")
 
 def generate_parameters(p_bit_len):
     """
-    Generates the domain (i.e., public) parameters to set up a key agreement between
-    parties.
+    Returns the domain (i.e., public) parameters p (the modulus), q (the subgroup
+    from which private exponents are selected and g (the generator of the subgroup)
+    to set up symmetric key agreement between 2 parties.
     """
     assert p_bit_len >= min_p_bit_len, f"Modulus p bit-length must be >= {min_p_bit_len}"
 
-    # Generate a 256-bit prime
-    q = primes.generate_prime(q_bit_len-1, q_bit_len)
+    # Generate a 256-bit prime, which is the upper bound of the subgroup to which
+    # all powers of the generator g will belong (and the subgroup from which
+    # parties will select private exponents).
+    q = primes.generate_prime(q_bit_len)
     
-    # Generate 
+    # Find an n and p satisfying the equation p = q*n+1, where p is prime, and
+    # the only non-trivial subgroup of p-1 is of size q, and consists of the elements
+    # 1, ..., q-1.
     n, p = generate_p(q, p_bit_len)
+    
+    # Find a generator g that generates all elements of the subgroup 1, ..., q-1.
     g = generate_g(q, n, p)
 
+    # Return the public parameters to be used by both parties for the DH setup.
     return p, q, g
 
 def generate_p(q, p_bit_len):
+    """
+    Return the numbers p and n that satisfy the equation p = q*n+1, where p is prime,
+    and the only non-trivial subgroup of p-1 is the size of q (the trivial subgroups
+    being a) the subgroup containing the number 1, b) the subgroup consisting of 1 and
+    p-1 and c) the full group of size q*n). It is to this non-trivial subgroup that any
+    public parameter that is a power of the generator g (e.g. public keys) will belong.
+    Since q is prime, it has no non-trivial subgroups.
+    """
+    # Compute lower and upper bounds from which to select a random n.
     n_bit_len = p_bit_len - q.bit_length()
     l, u = 2**(n_bit_len-1), 2**n_bit_len
 
-    p, n, i, tries = 4, 1, 0, 100 * math.floor(math.log2(u)+1)
-    while i < tries and not primes.is_prime(p):
-        i += 1
+    tries = 100 * math.floor(math.log2(u)+1)
+    for _ in range(tries):
         while True:
+            # Don't bother if n is not even (because if it is not, then p cannot be odd).
             n = secure_random.randrange(l, u)
             if n % 2 == 0:
                 break
 
         p = q * n + 1
-        if p.bit_length() < p_bit_len:
-            continue
-        print(p.bit_length(), n.bit_length(), q.bit_length())
 
+        # In addition to primality, check p for bit-length, as it might be less than
+        # that requested if n is small enough.
+        if p.bit_length() == p_bit_len and primes.is_prime(p):
+            break
+        
     return n, p
 
 def generate_g(q, n, p):
+    """
+    Returns a generator g, which is a primitive root of the group 1, ..., q-1;
+    that is, it generates every member of the group.
+    """
     while True:
-        a = secure_random.randrange(2, p - 2)
+        a = secure_random.randrange(2, p-2)
         g = primes.fast_mod_exp(a, n, p)
         if g != 1 and primes.fast_mod_exp(g, q, p) == 1:
             break
 
     return g
 
-def gen_keypair(g, q, p):
-    k_priv = secure_random.randrange(1, q - 1)
+def generate_keypair(g, q, p):
+    """
+    Returns a private key k_priv randomly selected from the group 1, ..., q-1, and
+    a public key of the form g**k_priv % p.
+    """
+    k_priv = secure_random.randrange(1, q-1)
     return k_priv, primes.fast_mod_exp(g, k_priv, p)
 
 def validate_pub_key(k, q, p):
-    assert 1 < k < p
-    assert primes.fast_mod_exp(k, q, p) == 1
+    """
+    Validates the public key.
+    """
+    assert 1 < k < p, f"Public key is out of range"
+    assert primes.fast_mod_exp(k, q, p) == 1, f"public key is invalid"
 
 def validate_parameters(p, q, g):
-    assert p.bit_length() >= min_p_bit_len, f"{p.bit_length()}"
-    assert q.bit_length() == q_bit_len
-    assert primes.is_prime(p)
-    assert primes.is_prime(q)
-    assert (p - 1) % q == 0
-    # TODO Make sure second test is not an unnecessary duplicate of the previous assert.
-    assert g != 1 and primes.fast_mod_exp(g, q, p) == 1
-
-def legendre_symbol(a, p):
-    """ Compute the Legendre symbol a|p using Euler's criterion. p is a prime, a is
-        relatively prime to p (if p divides a, then a|p = 0) Returns 1 if a has a
-        square root modulo p, -1 otherwise.
     """
-    ls = primes.fast_mod_exp(a, (p - 1) // 2, p)
-    return -1 if ls == p - 1 else ls
+    Validates the public parameters.
+    """
+    assert p.bit_length() >= min_p_bit_len, f"p.bit_length() {p.bit_length()} \
+        is less than min_p_bit_len {min_p_bit_len}"
+    assert q.bit_length() == q_bit_len, f"q.bit_length() {q.bit_length()} does \
+        not equal q_bit_len {q_bit_len}"
+    assert primes.is_prime(p), f"p is not prime"
+    assert primes.is_prime(q), f"q is not prime"
+    assert (p - 1) % q == 0, f"q is not a divisor of p-1"
+    # TODO Make sure second test is not an unnecessary duplicate of the previous assert.
+    assert g != 1 and primes.fast_mod_exp(g, q, p) == 1, f"g has illegal value"
 
 if __name__ == '__main__':
     main()
