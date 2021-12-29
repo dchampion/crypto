4ad4209b8c517abafa6cf9022a6e2315a12524a4
dchampion <divaconhamdip@gmail.com>
2021-12-29 16:03:15 -0700

    Fix typos

--- test/dh_test.py
+++ test/dh_test.py
@@ -6,8 +6,8 @@ import dh
 def main():
     print("Running dh tests...")
     test_dh_setup()
-    test_setup_encrypt_decrypt()
-    print("dh tests passed")
+    test_full_protocol()
+    print("all dh tests passed")
     
 def test_dh_setup():
     p, q, g = dh.generate_parameters(2048)
@@ -23,18 +23,18 @@ def test_dh_setup():
     kSession2 = dh.generate_session_key(kPub1, kPriv2, p)
     assert kSession1 == kSession2, "Secrets don't match"
 
-    print("dh setup passed")
+    print("test_dh_setup passed")
 
-def test_setup_encrypt_decrypt():
+def test_full_protocol():
     # Alice generates the public parameters for a DH session with Bob; these are the
     # public modulus [p] (a prime), the size of the subgroup modulo p within which
     # exchanged public keys must fall [q] (also a prime), and the generator of the
     # subgroup [g].
     p, q, g = dh.generate_parameters(dh.min_p_bit_len)
 
-    # Alice should validate these parameters before transmitting them to Bob; Bob
-    # must run the same validation of these parameters when he receives them from
-    # Alice.
+    # Alice may optionally validate these parameters before transmitting them to Bob.
+    # Bob, however, must validate them using the same funciton when he receives them
+    # from Alice.
     dh.validate_parameters(p, q, g)
 
     # Alice generates her private and public keys [kA, KA], using g, q and p as inputs.
@@ -56,7 +56,7 @@ def test_setup_encrypt_decrypt():
     dh.validate_pub_key(KA, q, p)
 
     # Bob generates his own private and public keys [kB, KB], using as inputs the public
-    # parameters [p, q, g] he received, and validated, from Alice.
+    # parameters [p, q, g] (now validated) that he received from Alice.
     kB, KB = dh.generate_keypair(g, q, p)
 
     ####################################################################################
@@ -68,14 +68,14 @@ def test_setup_encrypt_decrypt():
     ####################################################################################
 
     # Alice, having received Bob's public key [KB], uses it, along with her private key
-    # [kA] to generate a session key; this must be kept secret (i.e., it must not be
-    # transmitted to Bob, or otherwise exposed in any other way).
+    # [kA] to generate a session key [kSessionA]; this must be kept secret (i.e., it must
+    # not be transmitted to Bob or otherwise exposed in any other way).
     kSessionA = dh.generate_session_key(KB, kA, p)
 
     # Bob, having received Alice's public key [KA], uses it, along with his private key
-    # [kB] to generate a session key; this too must be kept secret. The session keys
-    # [kSessionA, kSessionB] that Alice and Bob compute should be equal, due to the
-    # properties of DH.
+    # [kB] to generate a session key [kSessionB]; this too must be kept secret. Due to 
+    # the essential properties of DH, the session keys [kSessionA, kSessionB] that Alice
+    # and Bob compute independently should be identical.
     kSessionB = dh.generate_session_key(KA, kB, p)
     assert kSessionA == kSessionB
 
@@ -97,9 +97,10 @@ def test_setup_encrypt_decrypt():
     mB = sym_decrypt(kSessionB, mAC)
     assert mA == str(mB)
 
-    print("full protocol (setup-encrypt-decrypt) test passed")
+    print("test_full_protocol passed")
 
 def sym_encrypt(key, msg):
+    # Poor man's symmetric cipher.
     return int.from_bytes(key, byteorder="little") ^ int(msg)
 
 def sym_decrypt(key, msg):
