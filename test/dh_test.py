ad18d2ec6510778f6950415ea36cf4c459a0a67f
dchampion <divaconhamdip@gmail.com>
2021-12-31 09:45:53 -0700

    Include protocol diagram

--- test/dh_test.py
+++ test/dh_test.py
@@ -26,6 +26,45 @@ def test_dh_setup():
     print("test_dh_setup passed")
 
 def test_full_protocol():
+    ########################################################################################
+    # The following diagram illustrates the protocol simulated in this test graphically.   #
+    # Values surrounded in square brackets [] are public (i.e., they can be transmitted    #
+    # over an insecure channel with no compromise to the integrity of the system if they   #
+    # are intercepted by an adversary), and those that are not private (i.e., they must be #
+    # kept secret). The direction arrows ---> and <--- indicate an exchange of information #
+    # between Alice and Bob on an insecure channel.                                        #
+    #                                                                                      #
+    # Alice                                       Bob                                      #
+    # ----------------------------                ----------------------------             #
+    # [p, q, g] =                                                                          #
+    #      generate_parameters(modulus_bit_length)                                         #
+    #                                                                                      #
+    # kA, [KA] =                                                                           #
+    #      generate_keypair(p, q, g)                                                       #
+    #                                                                                      #
+    # [p, q, g, KA]                     --->      [p, q, g, KA]                            #
+    #                                                  validate_parameters(p, q, g)        #
+    #                                                  validate_pub_key(KA)                #
+    #                                                                                      #
+    #                                             kB, [KB] =                               #
+    #                                                  generate_keypair(p, q, g)           #
+    #                                                                                      #
+    # [KB]                              <---      [KB]                                     #
+    #                                                                                      #
+    # kSessionA =                                 kSessionB =                              #
+    #      generate_session_key([KB], kA, [p])         generate_session_key([KA], kB, [p]) #
+    #                                                                                      #
+    # [mAC] =                                                                              #
+    #      sym_encrypt(kSessionA, mA)                                                      #
+    #                                                                                      #
+    # [mAC]                             --->      [mAC]                                    #
+    #                                                                                      #
+    #                                             mB =                                     #
+    #                                                  sym_decrypt(kSessionB, [mAC])       #
+    #                                                                                      #
+    # The message mB Bob decrypts must equal the message mA that Alice encrypted.          #
+    ########################################################################################
+
     # Alice generates the public parameters for a DH session with Bob; these are the
     # public modulus [p] (a prime), the size of the subgroup modulo p within which
     # exchanged public keys must fall [q] (also a prime), and the generator of the
@@ -37,17 +76,10 @@ def test_full_protocol():
     # from Alice.
     dh.validate_parameters(p, q, g)
 
-    # Alice generates her private and public keys [kA, KA], using g, q and p as inputs.
+    # Alice generates her private and public keys kA and [KA], respectively, using
+    # [g, q, p] as inputs. Alice transmits [p, q, g, KA] (but NOT her private key kA)
+    # to Bob.
     kA, KA = dh.generate_keypair(g, q, p)
-
-    ####################################################################################
-    # Alice, having generated the public parameters p, q and g, and her public key KA, #
-    # transmits all of these to Bob.                                                   #
-    #                                                                                  #
-    # Alice                        Bob                                                 #
-    # -------------                -------------                                       #
-    # [p, q, g, KA]      --->      [p, q, g, KA]                                       #
-    ####################################################################################
 
     # Bob MUST validate the public parameters [p, q, g] he receives from Alice.
     dh.validate_parameters(p, q, g)
@@ -55,45 +87,29 @@ def test_full_protocol():
     # Bob MUST ALSO validate Alice's public key [KA].
     dh.validate_pub_key(KA, q, p)
 
-    # Bob generates his own private and public keys [kB, KB], using as inputs the public
-    # parameters [p, q, g] (now validated) that he received from Alice.
+    # Bob generates his own private and public keys kB and [KB], using as inputs the
+    # public parameters [p, q, g] (now validated) he received from Alice. Bob transmits
+    # his public key [KB] (but NOT his private key kB) to Alice.
     kB, KB = dh.generate_keypair(g, q, p)
 
-    ####################################################################################
-    # Bob transmits his public key [KB] to Alice.                                      #
-    #                                                                                  #
-    # Alice                        Bob                                                 #
-    # -------------                -------------                                       #
-    # [KB]               <---      [KB]                                                #
-    ####################################################################################
-
     # Alice, having received Bob's public key [KB], uses it, along with her private key
-    # [kA] to generate a session key [kSessionA]; this must be kept secret (i.e., it must
-    # not be transmitted to Bob or otherwise exposed in any other way).
+    # kA to generate a session key kSessionA, which must be kept secret.
     kSessionA = dh.generate_session_key(KB, kA, p)
 
     # Bob, having received Alice's public key [KA], uses it, along with his private key
-    # [kB] to generate a session key [kSessionB]; this too must be kept secret. Due to 
-    # the essential properties of DH, the session keys [kSessionA, kSessionB] that Alice
-    # and Bob compute independently should be identical.
+    # kB to generate a session key kSessionB, which must must be kept secret. Due to 
+    # the essential property of DH, the session keys kSessionA and kSessionB, that
+    # Alice and Bob have computed independently, should be identical.
     kSessionB = dh.generate_session_key(KA, kB, p)
     assert kSessionA == kSessionB
 
-    # Alice produces a message [mA], encrypts it using her session key [kSessionA], and
+    # Alice produces a message [mA], encrypts it using her session key kSessionA, and
     # transmits the ciphertext [mAC] to Bob.
     mA = "8675309"
     mAC = sym_encrypt(kSessionA, mA)
 
-    ####################################################################################
-    # Alice transmits encrypted message [mAC] to Bob.                                  #
-    #                                                                                  #
-    # Alice                        Bob                                                 #
-    # -------------                -------------                                       #
-    # [mAC]               --->     [mAC]                                               #
-    ####################################################################################
-
-    # Bob receives the ciphertext [mAC], and decrypts it using his session key [kSessionB].
-    # The message Bob decrypts [mB] must equal the message Alice encrypted [mA].
+    # Bob receives the ciphertext [mAC], and decrypts it using his session key kSessionB.
+    # The message Bob decrypts mB must equal the message Alice encrypted mA.
     mB = sym_decrypt(kSessionB, mAC)
     assert mA == str(mB)
 
