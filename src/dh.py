87e984a44d4fd9ed14ba7d816adb501a2406dc9a
dchampion <divaconhamdip@gmail.com>
2022-01-03 09:48:33 -0700

    Clean up parameter validation

--- src/dh.py
+++ src/dh.py
@@ -1,4 +1,4 @@
-""" Various functions providing Diffie-Hellman symmetric key agreement. """
+""" An implementation of Diffie-Hellman """
 import primes
 import secrets
 import math
@@ -13,16 +13,17 @@ min_p_bit_len = 2048
 
 def generate_parameters(p_bit_len):
     """
-    Returns the domain (i.e., public) parameters [p, q, g] for the shared,
-    symmetric (private) key negotiation. These are the modulus [p], the
-    order (or size) of a large subgroup modulo p [q], and a generator of
-    that subgroup [g].
+    Returns the domain (i.e., public) parameters [q, p, g] required for two
+    parties to negotiate a shared, private key to be used to encrypt data using
+    a symmetric cipher (e.g., AES). These are the order (or size) of a large
+    subgroup modulo p [q], the modulus [p], and a generator of the subgroup
+    modulo p [g].
     """
-    assert p_bit_len >= min_p_bit_len, f"p_bit_len must be >= {min_p_bit_len}"
+    assert isinstance(p_bit_len, int) and p_bit_len >= min_p_bit_len
 
     # Generate q; a 256-bit prime, which will be the order (or size) of a
     # large subgroup modulo p within which the public keys exchanged between
-    # communicating parties will fall.
+    # communicating parties must fall.
     q = primes.generate_prime(q_bit_len)
     
     # Find values for n and p that satisfy the equation p = q * n + 1, where
@@ -31,21 +32,24 @@ def generate_parameters(p_bit_len):
     # subgroup of order (or size) q. The order of this subgroup must be large
     # (256 bits, to provide 128-bit security) in order to thwart collision-
     # style attacks.
-    n, p = generate_p(q, p_bit_len)
+    n, p = _generate_p(q, p_bit_len)
 
     # Find a generator g that generates all elements of the subgroup of order
     # (or size) q.
-    g = generate_g(n, p)
+    g = _generate_g(n, p)
 
-    return p, q, g
+    return q, p, g
 
-def generate_p(q, p_bit_len):
+def _generate_p(q, p_bit_len):
     """
-    Returns the numbers [n, p] that satisfy the equation p = q * n + 1, where p is
-    a prime DH modulus of length p_bit_len, and n will be used in the computation
-    of a generator of the subroup modulo p that is of order (or size) q (see
-    function genetate_g).
+    Returns positive integers [n, p] that satisfy the equation p = q * n + 1, where
+    p will serve as a prime modulus of length p_bit_len. The value n returned from
+    this function will be used as input to the computation of a generator of the
+    subgroup modulo p that is of order (or size) q.
     """
+    assert isinstance(q, int) and q.bit_length() >= q_bit_len
+    assert isinstance(p_bit_len, int) and p_bit_len >= min_p_bit_len
+
     # Compute bounds from which to select a random factor n.
     n_bit_len = p_bit_len - q.bit_length()
     l, u = 2**(n_bit_len-1), 2**n_bit_len
@@ -65,22 +69,26 @@ def generate_p(q, p_bit_len):
         if primes.is_prime(p):
             break
 
-    assert i < tries-1, "unable to generate a suitable prime"
+    if i == tries - 1:
+        raise Exception("Unable to generate a suitable prime")
 
     return n, p
 
-def generate_g(n, p):
+def _generate_g(n, p):
     """
-    Returns a generator [g] that generates the entire subgroup modulo p of order
+    Returns a generator g that generates the entire subgroup modulo p of order
     (or size) q.
     """
+    assert isinstance(p, int) and p.bit_length() >= min_p_bit_len - 1
+    assert isinstance(n, int) and (p - 1) % n == 0
+
     secure_random = secrets.SystemRandom()
     while True:
         # Pick a random base in the full range of the modulus.
         a = secure_random.randrange(2, p-2)
 
         # Any such base raised to the power of n modulo p should yield either 1,
-        # or a generator of the entire group of order (or size) q.
+        # or a generator of the entire subgroup of order (or size) q.
         g = util.fast_mod_exp(a, n, p)
 
         # If g is not 1, it must be a generator of the subgroup.
@@ -89,51 +97,63 @@ def generate_g(n, p):
 
     return g
 
-def generate_keypair(g, q, p):
+def generate_keypair(q, p, g):
     """
     Returns a private key randomly selected from the set (1, ..., q-1), and a
-    public key of the form (g**kPriv) % p, where g is the generator of the subgroup
-    modulo p of order (or size) q. The private key [kPriv] returned by this function
+    public key of the form (g**k_prv) % p, where g is the generator of the subgroup
+    modulo p of order (or size) q. The private key k_prv returned by this function
     must be kept secret.
     """
+    _validate_parameters(q, p, g)
+
     # Select a random, private key in the range of q.
     secure_random = secrets.SystemRandom()
-    kPriv = secure_random.randrange(1, q-1)
+    k_prv = secure_random.randrange(1, q-1)
 
     # Compute a public key based on this private key.
-    kPub = util.fast_mod_exp(g, kPriv, p)
+    k_pub = util.fast_mod_exp(g, k_prv, p)
 
     # Validate the publid key.
-    validate_pub_key(kPub, q, p)
+    validate_pub_key(k_pub, q, p)
 
-    return kPriv, kPub
+    return k_prv, k_pub
 
-def generate_session_key(kPub, kPriv, p):
+def generate_session_key(k_pub, k_prv, p):
     """
     Returns a hashed byte array to be used as a session key in a symmetric cipher
-    that is agreed upon by both parties in the setup phase. This key must be kept
-    secret.
+    agreed upon by both parties in the setup phase. This key must be kept secret.
     """
+    assert isinstance(k_pub, int)
+    assert isinstance(k_prv, int)
+    assert isinstance(p, int) and p.bit_length() >= min_p_bit_len - 1
+
     # Compute a session key using the essential property of DH (i.e., raising
-    # the other communicant's public key to the power of this communicant's
-    # private key modulo p).
-    ki = util.fast_mod_exp(kPub, kPriv, p)
+    # the other party's public key to the power of this party's private key modulo p).
+    ki = util.fast_mod_exp(k_pub, k_prv, p)
 
     # The session key is hashed in order to obscure any mathematical structure
-    # in ki that could be exploited by an attacker if it were to be leaked.
+    # in ki that could be exploited by an adversary if it were to be leaked.
     return _hash_key(ki)
 
 def _hash_key(k):
     """
-    Returns the input k (a large integer) as a hashed byte array.
+    Returns a hashed byte array of the positive integer k.
     """
+    assert isinstance(k, int)
+
     return hashlib.sha256(str(k).encode()).digest()
 
 def validate_pub_key(k, q, p):
     """
-    Validates the public key k. Must be called without raising an exception
-    by a receiving party before proceding with a session.
+    Validates a public key k given the public parameters q and p. Must be called,
+    without raising an exception, by a receiving party before proceding with a session.
+    If this function raises an exception, the public key is invalid and the protocol should
+    be halted.
     """
+    assert isinstance(k, int)
+    assert isinstance(q, int)
+    assert isinstance(p, int)
+
     # Check that
     # (1) k is in range, and
     # (2) k is in the subgroup of order (or size) q.
@@ -145,11 +165,16 @@ def validate_pub_key(k, q, p):
     if not valid:
         raise Exception("Invalid key")
 
-def validate_parameters(p, q, g):
+def _validate_parameters(q, p, g):
     """
-    Validates the public parameters [p, q, g]. Must be called without raising
-    an exception by a receiving party before proceeding with a session.
+    Validates the public parameters [q, p, g]. Must be called, without raising
+    an exception, by a receiving party before proceeding with a session. If this
+    function raises an exception, the parameters are invalid and the protocol should
+    be halted.
     """
+    assert isinstance(q, int)
+    assert isinstance(p, int)
+    assert isinstance(g, int)
 
     # Check that
     # (1) the bit length of the modulus [p] is as big as that requested,
--- test/dh_test.py
+++ test/dh_test.py
@@ -10,18 +10,18 @@ def main():
     print("all dh tests passed")
     
 def test_dh_setup():
-    p, q, g = dh.generate_parameters(2048)
-    dh.validate_parameters(p, q, g)
+    q, p, g = dh.generate_parameters(2048)
+    dh._validate_parameters(q, p, g)
     
-    kPriv1, kPub1 = dh.generate_keypair(g, q, p)
-    dh.validate_pub_key(kPub1, q, p)
+    k_prv_1, k_pub_1 = dh.generate_keypair(q, p, g)
+    dh.validate_pub_key(k_pub_1, q, p)
 
-    kPriv2, kPub2 = dh.generate_keypair(g, q, p)
-    dh.validate_pub_key(kPub2, q, p)
+    k_prv_2, k_pub_2 = dh.generate_keypair(q, p, g)
+    dh.validate_pub_key(k_pub_2, q, p)
 
-    kSession1 = dh.generate_session_key(kPub2, kPriv1, p)
-    kSession2 = dh.generate_session_key(kPub1, kPriv2, p)
-    assert kSession1 == kSession2, "Secrets don't match"
+    k_sess_1 = dh.generate_session_key(k_pub_2, k_prv_1, p)
+    k_sess_2 = dh.generate_session_key(k_pub_1, k_prv_2, p)
+    assert k_sess_1 == k_sess_2, "Secrets don't match"
 
     print("test_dh_setup passed")
 
@@ -36,18 +36,18 @@ def test_full_protocol():
     #                                                                                      #
     # Alice                                       Bob                                      #
     # ----------------------------                ----------------------------             #
-    # [p, q, g] =                                                                          #
+    # [q, p, g] =                                                                          #
     #      generate_parameters(modulus_bit_length)                                         #
     #                                                                                      #
     # kA, [KA] =                                                                           #
-    #      generate_keypair(p, q, g)                                                       #
+    #      generate_keypair(q, p, g)                                                       #
     #                                                                                      #
-    # [p, q, g, KA]                     --->      [p, q, g, KA]                            #
-    #                                                  validate_parameters(p, q, g)        #
+    # [p, q, g, KA]                     --->      [q, p, g, KA]                            #
+    #                                                  validate_parameters(q, p, g)        #
     #                                                  validate_pub_key(KA)                #
     #                                                                                      #
     #                                             kB, [KB] =                               #
-    #                                                  generate_keypair(p, q, g)           #
+    #                                                  generate_keypair(q, p, g)           #
     #                                                                                      #
     # [KB]                              <---      [KB]                                     #
     #                                                                                      #
@@ -69,20 +69,20 @@ def test_full_protocol():
     # public modulus [p] (a prime), the size of the subgroup modulo p within which
     # exchanged public keys must fall [q] (also a prime), and the generator of the
     # subgroup [g].
-    p, q, g = dh.generate_parameters(dh.min_p_bit_len)
+    q, p, g = dh.generate_parameters(dh.min_p_bit_len)
 
     # Alice may optionally validate these parameters before transmitting them to Bob.
     # Bob, however, must validate them using the same funciton when he receives them
     # from Alice.
-    dh.validate_parameters(p, q, g)
+    dh._validate_parameters(q, p, g)
 
     # Alice generates her private and public keys kA and [KA], respectively, using
-    # [g, q, p] as inputs. Alice transmits [p, q, g, KA] (but NOT her private key kA)
+    # [g, q, p] as inputs. Alice transmits [q, p, g, KA] (but NOT her private key kA)
     # to Bob.
-    kA, KA = dh.generate_keypair(g, q, p)
+    kA, KA = dh.generate_keypair(q, p, g)
 
     # Bob MUST validate the public parameters [p, q, g] he receives from Alice.
-    dh.validate_parameters(p, q, g)
+    dh._validate_parameters(q, p, g)
 
     # Bob MUST ALSO validate Alice's public key [KA].
     dh.validate_pub_key(KA, q, p)
@@ -90,7 +90,7 @@ def test_full_protocol():
     # Bob generates his own private and public keys kB and [KB], using as inputs the
     # public parameters [p, q, g] (now validated) he received from Alice. Bob transmits
     # his public key [KB] (but NOT his private key kB) to Alice.
-    kB, KB = dh.generate_keypair(g, q, p)
+    kB, KB = dh.generate_keypair(q, p, g)
 
     # Alice, having received Bob's public key [KB], uses it, along with her private key
     # kA to generate a session key kSessionA, which must be kept secret.
