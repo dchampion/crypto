9866f7a3baef7a32d01094a5424ebaebcc4d949e
dchampion <divaconhamdip@gmail.com>
2022-08-15 14:43:18 -0600

    Clarify doc, and add a performance enhancement

--- src/dh.py
+++ src/dh.py
@@ -7,28 +7,31 @@ import primes
 import util
 import prng
 
-# Bit length of a large subgroup modulo p (where p is a prime modulus).
+# Bit length of a prime number q that is the order (or size) of large subgroup modulo
+# p, where p is a (much larger) prime number that is the order of the full group modulo p.
 _q_bit_len = 256
 
-# Minimum bit length of a prime modulus.
-_min_p_bit_len = 2048
+# Minimum bit length of a prime modulus p.
+_p_min_bit_len = 2048
 
 def generate_parameters(p_bit_len: int) -> tuple[int, int, int]:
     """
     Returns the public parameters necessary for two parties to negotiate a shared,
-    private key for use in a symmetric cipher (e.g., 3DES, AES). The returned value
-    is a tuple of the form (q, p, g), where q is the order (or size) of a large
-    subgroup modulo p, p is a prime modulus, and g is a generator of the subgroup.
+    private key to be used in a symmetric cipher (e.g., 3DES, AES). The returned
+    value is a tuple of the form (q, p, g), where q is a 256-bit prime number that
+    is the order of the smallest subgroup modulo p, p is a prime number of at least
+    p_bit_len length (p_bit_len must be at least 2048 bits) that is the order of
+    the full group modulo p, and g is a generator of the subgroup of order q.
     """
-    assert isinstance(p_bit_len, int) and p_bit_len >= _min_p_bit_len
+    assert isinstance(p_bit_len, int) and p_bit_len >= _p_min_bit_len
 
-    # Generate q; a 256-bit prime, which will be the order (or size) of a
-    # large subgroup modulo p within which the public keys exchanged between
-    # communicating parties must fall.
+    # Generate a 256-bit prime that will be the order (or size) of a large subgroup
+    # modulo p, within which the public keys exchanged between communicating parties
+    # must fall.
     q = primes.generate_prime(_q_bit_len)
     
     # Find values for n and p that satisfy the equation p = q * n + 1, where
-    # p (a p_bit_len prime), will serve as the prime modulus. This ensures that
+    # p (a p_bit_len prime) will serve as the prime modulus. This ensures that
     # the public keys used in the secret key agreement will fall into the
     # subgroup of order (or size) q. The order of this subgroup must be large
     # (256 bits) to thwart collision-style attacks.
@@ -46,7 +49,7 @@ def _generate_p(q: int, p_bit_len: int) -> tuple[int, int]:
     # this function will be used to compute a generator of the subgroup modulo p
     # that is of order (or size) q.
     assert isinstance(q, int) and q.bit_length() >= _q_bit_len
-    assert isinstance(p_bit_len, int) and p_bit_len >= _min_p_bit_len
+    assert isinstance(p_bit_len, int) and p_bit_len >= _p_min_bit_len
 
     # Compute bounds from which to select a random factor n.
     n_bit_len = p_bit_len - q.bit_length()
@@ -55,9 +58,10 @@ def _generate_p(q: int, p_bit_len: int) -> tuple[int, int]:
     tries = 100 * p_bit_len
     for i in range(tries):
         n = prng.randrange(l, u)
-        p = q * n + 1
-        if primes.is_prime(p):
-            break
+        if n % 2 == 0: # n must be even for p to be prime, so skip if n is odd.
+            p = q * n + 1
+            if primes.is_prime(p):
+                break
 
     if i == tries - 1:
         raise Exception("Unable to generate a suitable prime")
@@ -67,7 +71,7 @@ def _generate_p(q: int, p_bit_len: int) -> tuple[int, int]:
 def _generate_g(n: int, p: int) -> int:
     # Returns a generator g that generates the entire subgroup modulo p of order
     # (or size) q.
-    assert isinstance(p, int) and p.bit_length() >= _min_p_bit_len - 1
+    assert isinstance(p, int) and p.bit_length() >= _p_min_bit_len - 1
     assert isinstance(n, int) and (p - 1) % n == 0
 
     while True:
@@ -75,7 +79,7 @@ def _generate_g(n: int, p: int) -> int:
         a = prng.randrange(2, p-2)
 
         # Any such base raised to the power of n modulo p should yield either 1,
-        # or a generator of the entire subgroup of order (or size) q.
+        # or else a generator of the entire subgroup of order (or size) q.
         g = util.fast_mod_exp(a, n, p)
 
         # If g is not 1, it must be a generator of the subgroup.
@@ -86,11 +90,12 @@ def _generate_g(n: int, p: int) -> int:
 
 def generate_keypair(q: int, p: int, g: int) -> tuple[int, int]:
     """
-    Given the public parameters q, p and g, returns a tuple of the form (k_prv, k_pub),
+    Given the public parameters q, p and g (which are obtained from this module's
+    generate_parameters function), returns a tuple of the form (k_prv, k_pub),
     where k_prv is a private key randomly selected from the range (1, ..., q-1),
     and k_pub is a public key of the form g^k_prv % p. In this equation, g is a
-    generator of the subgroup modulo p that is of order (or size) q. The private key
-    k_prv returned by this function must be kept secret; whereas the public key k_pub
+    generator of the subgroup modulo p that is of order q. The private key k_prv
+    returned by this function must be kept secret; whereas the public key k_pub
     may be shared freely.
     """
     validate_parameters(q, p, g)
@@ -108,12 +113,14 @@ def generate_keypair(q: int, p: int, g: int) -> tuple[int, int]:
 
 def generate_session_key(k_pub: int, k_prv: int, p: int) -> bytes:
     """
-    Returns a hashed byte array suitable for use as a session key in a symmetric
-    cipher (e.g. 3DES, AES). This key must be kept secret.
+    Given a private key k_prv known only to the caller of this function, and a
+    public key k_pub supplied by another party, returns a hashed byte array suitable
+    for use as a session key to be used by both parties in a symmetric cipher (e.g.,
+    3DES, AES). The session key returned by this function must be kept secret.
     """
     assert isinstance(k_pub, int)
     assert isinstance(k_prv, int)
-    assert isinstance(p, int) and p.bit_length() >= _min_p_bit_len - 1
+    assert isinstance(p, int) and p.bit_length() >= _p_min_bit_len - 1
 
     # Compute a session key using the essential property of DH (i.e., by raising
     # the other party's public key to the power of this party's private key modulo p).
@@ -123,25 +130,25 @@ def generate_session_key(k_pub: int, k_prv: int, p: int) -> bytes:
     # exploited by an adversary if it were to be leaked.
     return util.hash(ki)
 
-def validate_pub_key(k: int, q: int, p: int) -> None:
+def validate_pub_key(k_pub: int, q: int, p: int) -> None:
     """
-    Validates a public key k given the public parameters q and p. Must be called,
-    without raising an exception, by a party receiving the public key and parameters from
-    another party before proceding with a session. If this function raises an exception,
-    the public key is invalid and the protocol should be halted.
+    Validates a public key k_pub given its corresponding public parameters q and p. Must be
+    called, without raising an exception, by a party receiving the public key and parameters
+    from another party. If this function raises an exception, the public key is invalid and
+    the session should be halted.
     """
-    assert isinstance(k, int)
+    assert isinstance(k_pub, int)
     assert isinstance(q, int)
     assert isinstance(p, int)
 
     valid = True
 
-    # k must be in the interval [2, p-1].
-    if valid and k <= 1 or k >= p:
+    # k_pub must be in the interval [2, p-1].
+    if valid and k_pub <= 1 or k_pub >= p:
         valid = False
 
-    # k must be in the subgroup of order (or size) q.
-    if valid and util.fast_mod_exp(k, q, p) != 1:
+    # k_pub must be in the subgroup of order (or size) q.
+    if valid and util.fast_mod_exp(k_pub, q, p) != 1:
         valid = False
 
     if not valid:
@@ -151,8 +158,9 @@ def validate_parameters(q: int, p: int, g: int) -> None:
     """
     Validates the public parameters q, p and g returned from the function generate_parameters.
     Must be called, without raising an exception, by a party receiving these parameters from
-    another party before proceeding with a session. If this function raises an exception,
-    the parameters are invalid and the protocol should be halted.
+    another party, or by a party generating the parameters using this module's generate_parameters
+    function. If this function raises an exception, the parameters are invalid and the
+    session should be halted.
     """
     assert isinstance(q, int)
     assert isinstance(p, int)
@@ -162,10 +170,10 @@ def validate_parameters(q: int, p: int, g: int) -> None:
 
     # The bit length of modulus p must be greater than _min_p_bit_len - 1
     # (multiplication of 2 x-bit integers can produce a (2x-1)-bit result).
-    if valid and p.bit_length() < _min_p_bit_len - 1:
+    if valid and p.bit_length() < _p_min_bit_len - 1:
         valid = False
 
-    # The bit length of q must be equal to q_bit_len.
+    # The bit length of q must be equal to _q_bit_len.
     if valid and q.bit_length() != _q_bit_len:
         valid = False
 
