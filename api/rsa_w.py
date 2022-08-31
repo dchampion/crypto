a5dbb336bf3177caffe83e58e533c3ea86722a4d
dchampion <divaconhamdip@gmail.com>
2022-08-31 16:06:26 -0600

    Fix docstring

--- api/rsa_w.py
+++ api/rsa_w.py
@@ -6,8 +6,9 @@ from src import rsa
 def construct(bit_len: int) -> tuple[RsaKey, RsaKey]:
 
     """
-    Given the size of an RSA modulus in bits (bit_len), returns a Crypto.PublicKey.RSA.RsaKey
-    (see https://www.pycryptodome.org/src/public_key/rsa# for relevant documentation and examples).
+    Given the size of an RSA modulus in bits (bit_len), returns a pair of Crypto.PublicKey.RSA.RsaKey(s);
+    one for signing and the other for encryption. (see https://www.pycryptodome.org/src/public_key/rsa#
+    for relevant documentation and examples).
     """
 
     p, q, n, d3, d5 = rsa.generate_rsa_key(bit_len)
