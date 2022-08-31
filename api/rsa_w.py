43a98b0750f3b6f531c4d668a4532d4be99d3183
dchampion <divaconhamdip@gmail.com>
2022-08-31 10:33:52 -0600

    Fix formatting issue

--- api/rsa_w.py
+++ api/rsa_w.py
@@ -7,5 +7,5 @@ def construct(bit_len: int) -> tuple[RsaKey, RsaKey]:
 
     p, q, n, d3, d5 = rsa.generate_rsa_key(bit_len)
 
-    return RSA.construct((n, rsa.ENCRYPTION_EXPONENT, d5, p, q)),\
+    return RSA.construct((n, rsa.ENCRYPTION_EXPONENT,   d5, p, q)),\
            RSA.construct((n, rsa.VERIFICATION_EXPONENT, d3, p, q))
